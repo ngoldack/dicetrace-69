36d94c91ec897cd10e1b33bf03ad4fd74c5665b4
ngoldack <nicolas.goldack@proton.me>
2023-11-09 19:37:12 +0100

    wip

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,23 +1,6 @@
-import { dev } from '$app/environment';
-import { env } from '$env/dynamic/public';
 import { errorSchema } from '$lib/schemas/error';
 import type { HandleClientError } from '@sveltejs/kit';
-import { H } from 'highlight.run';
-import { version } from 'vite';
 import { createId } from '@paralleldrive/cuid2';
-
-H.init(env.PUBLIC_HIGHLIGHT_PROJECT_ID, {
-	environment: dev ? 'development' : 'production',
-	version: version,
-	serviceName: 'dicetrace',
-	storageMode: 'sessionStorage',
-	manualStart: false,
-	tracingOrigins: true,
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true
-	}
-});
 
 export const handle: HandleClientError = async ({ error }) => {
 	const parsed = errorSchema.safeParse(error);
